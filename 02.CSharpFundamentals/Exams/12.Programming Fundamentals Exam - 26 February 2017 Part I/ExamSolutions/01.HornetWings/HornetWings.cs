7546007ac25c6606270455a8b9e10d03fade4b92
NedNikolov <nedyalkonikolov92@gmail.com>
2018-06-22 16:27:12 +0300

    Exam added

---
+++ 02.CSharpFundamentals/Exams/12.Programming Fundamentals Exam - 26 February 2017 Part I/ExamSolutions/01.HornetWings/HornetWings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.HornetWings
+{
+    class HornetWings
+    {
+        static void Main(string[] args)
+        {
+            long wingFlaps = long.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            long endurance = long.Parse(Console.ReadLine());
+
+            double resultMetres = (wingFlaps / 1000) * distance;
+            Console.WriteLine($"{resultMetres:F2} m.");
+
+            long hornetFlaps = wingFlaps / 100;
+            long result = hornetFlaps + ((wingFlaps / endurance) * 5);
+            Console.WriteLine($"{result} s.");
+
+        }
+    }
+}
---
+++ 02.CSharpFundamentals/Exams/12.Programming Fundamentals Exam - 26 February 2017 Part I/ExamSolutions/02.HornetComm/HornetComm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.HornetComm
+{
+    class HornetComm
+    {
+        static void Main(string[] args)
+        {
+            List<string> messages = new List<string>();
+            List<string> broadcasts = new List<string>();
+
+            while (true)
+            {               
+                var input = Console.ReadLine().Split(new string[] {" <-> "}, StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "Hornet is Green")
+                {
+                    break;
+                }
+                if (input.Length != 2)
+                {
+                    input = Console.ReadLine().Split(new string[] { " <-> " }, StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+
+                string leftPart = input[0];
+                string rightPart = input[1];
+
+                if (leftPart.All(char.IsDigit) && rightPart.All(char.IsLetterOrDigit))
+                {
+                    string reversed = new string(leftPart.ToCharArray().Reverse().ToArray());
+                    messages.Add($"{reversed} -> {rightPart}");
+                }
+                else if (leftPart.All(c => !char.IsDigit(c)) && rightPart.All(char.IsLetterOrDigit))
+                {
+                    string reversedCases = ReversedCases(rightPart);
+                    broadcasts.Add($"{reversedCases} -> {leftPart}");
+                }
+
+            }
+
+            Console.WriteLine("Broadcasts:");
+            Console.WriteLine(broadcasts.Count == 0 ? "None" : string.Join(Environment.NewLine, broadcasts));
+            Console.WriteLine("Messages:");
+            Console.WriteLine(messages.Count == 0 ? "None" : string.Join(Environment.NewLine, messages));
+        }
+
+        private static string ReversedCases(string rightPart)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < rightPart.Length; i++)
+            {
+                if (char.IsUpper(rightPart[i]))
+                {
+                    result.Append(char.ToLower(rightPart[i]));
+                }
+                else if (char.IsLower(rightPart[i]))
+                {
+                    result.Append(char.ToUpper(rightPart[i]));
+                }
+                else
+                {
+                    result.Append(rightPart[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
