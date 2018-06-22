5c97927ebd9b26961b2aa4c0375dcdc6e71d7dd3
NedNikolov <nedyalkonikolov92@gmail.com>
2018-06-22 16:33:23 +0300

    Added exams

---
+++ 02.CSharpFundamentals/Exams/13.Programming Fundamentals Exam - 26 February 2017 Part II/ExamSolution/01.HornetAssault/HornetAssault.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.HornetAssault
+{
+    class HornetAssault
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 02.CSharpFundamentals/Exams/13.Programming Fundamentals Exam - 26 February 2017 Part II/ExamSolution/02.HornetArmada/HornetArmada.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.HornetArmada
+{
+    class HornetArmada
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
