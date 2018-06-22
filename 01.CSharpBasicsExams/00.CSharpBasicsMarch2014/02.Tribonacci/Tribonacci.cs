9f6d0f8683779ca6e29c7bae0a66368745ea604c
NedNikolov <nedyalkonikolov92@gmail.com>
2018-06-22 12:39:52 +0300

    added exams

---
+++ 01.CSharpBasicsExams/00.CSharpBasicsMarch2014/02.Tribonacci/Tribonacci.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Tribonacci
+{
+    class Tribonacci
+    {
+        static void Main(string[] args)
+        {
+            BigInteger t1 = long.Parse(Console.ReadLine());
+            BigInteger t2 = long.Parse(Console.ReadLine());
+            BigInteger t3 = long.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1)
+            {
+                Console.WriteLine(t1);
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine(t2);
+            }
+            else if (n == 3)
+            {
+                Console.WriteLine(t3);
+            }
+            else
+            {
+                BigInteger tn = 0;
+
+                for (int i = 4; i <= n; i++)
+                {
+                    tn = t1 + t2 + t3;
+                    t1 = t2;
+                    t2 = t3;
+                    t3 = tn;
+                }
+                Console.WriteLine(tn);
+            }
+        }
+    }
+}
---
+++ 01.CSharpBasicsExams/00.CSharpBasicsMarch2014/04.BullsAndCows/BullsAndCows.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class BullsAndCows
+{
+    static void Main()
+    {
+        //int secretNumber = 2228;
+        //int bulls = 2;
+        //int cows = 1;
+        //for (int guessNumber = 1111; guessNumber < 9999; guessNumber++)
+        //{
+        //    int bullFound = GetBulls(secretNumber, guessNumber);
+        //    int cowFound = GetCows(secretNumber, guessNumber);
+        //    if (bulls== bullFound && cows == cowFound)
+        //    {
+
+        //        Console.Write(guessNumber);
+        //        Console.Write(" ");
+        //    }
+
+        //}
+        Console.WriteLine(GetCows(1234,1345));
+    }
+
+    private static string GetCows(int secretNumber, int guessNumber)
+    {
+        string secretNum = secretNumber.ToString().PadLeft(4, '0');
+        string guessNum = guessNumber.ToString().PadLeft(4, '0');
+        int bullCount = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (secretNum[i] == guessNum[i])
+            {
+                secretNum[i] = '*';
+                guessNum[i] = '*';
+            }
+        }
+        return bullCount;
+    }
+
+    private static int GetBulls(int secretNumber,int guessNumber)
+    {
+        string secretNum = secretNumber.ToString().PadLeft(4, '0');
+        string guessNum = guessNumber.ToString().PadLeft(4, '0');
+        int bullCount = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (secretNum[i] == guessNum[i])
+            {
+                bullCount++;
+            }
+        }
+        return bullCount;
+    }
+}
+
