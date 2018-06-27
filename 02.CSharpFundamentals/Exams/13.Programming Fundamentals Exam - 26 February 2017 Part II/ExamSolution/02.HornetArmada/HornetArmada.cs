5f3d8cafe6b06aeecd6713b18b886078f2e05fda
NedNikolov <nedyalkonikolov92@gmail.com>
2018-06-27 11:29:18 +0300

    added solution

--- 02.CSharpFundamentals/Exams/13.Programming Fundamentals Exam - 26 February 2017 Part II/ExamSolution/02.HornetArmada/HornetArmada.cs
+++ 02.CSharpFundamentals/Exams/13.Programming Fundamentals Exam - 26 February 2017 Part II/ExamSolution/02.HornetArmada/HornetArmada.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.HornetArmada
 {
@@ -6,7 +8,68 @@ namespace _02.HornetArmada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> legionActivity = new Dictionary<string, int>();
+
+            Dictionary<string, Dictionary<string, long>> soldiers = new Dictionary<string, Dictionary<string, long>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(new char[] {'=', '-', '>', ':', ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                int activity = int.Parse(tokens[0]);
+                string legionName = tokens[1];
+                string type = tokens[2];
+                long count = long.Parse(tokens[3]);
+
+                if (!legionActivity.ContainsKey(legionName))
+                {
+                    legionActivity.Add(legionName, activity);
+                    soldiers.Add(legionName, new Dictionary<string, long>());
+                }
+
+                if (!soldiers[legionName].ContainsKey(type))
+                {
+                    soldiers[legionName][type] = 0;
+                }
+
+                if (legionActivity[legionName] < activity)
+                {
+                    legionActivity[legionName] = activity;
+                }
+
+                soldiers[legionName][type] += count;
+
+            }
+
+            string[] conditions = Console.ReadLine().Split('\\');
+
+            if (conditions.Length == 1)
+            {
+                string soldierType = conditions[0];
+                foreach (KeyValuePair<string, int> legion in legionActivity.OrderByDescending(l => l.Value))
+                {
+                    if (soldiers[legion.Key].ContainsKey(soldierType))
+                    {
+                        Console.WriteLine($"{legion.Value} : {legion.Key}");
+                    }
+                }
+            }
+            else
+            {
+                int searchedActivity = int.Parse(conditions[0]);
+                string searchedsoldiers = conditions[1];
+
+                foreach (var legion in soldiers.Where(l => l.Value.ContainsKey(searchedsoldiers)).OrderByDescending(l => l.Value[searchedsoldiers]))
+                {
+                    if (legionActivity[legion.Key] < searchedActivity)
+                    {
+                        Console.WriteLine($"{legion.Key} -> {legion.Value[searchedsoldiers]}");
+                    }                    
+                }
+            }
         }
     }
 }
